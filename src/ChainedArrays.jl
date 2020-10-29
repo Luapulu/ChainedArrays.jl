25713ef56c1676b0667467e7fa4396fa1ff017bf
Paul Nemec <paul.nemec@tum.de>
2020-10-29 14:13:49 +0100

    Minimal working example

--- src/ChainedArrays.jl
+++ src/ChainedArrays.jl
@@ -1,5 +1,61 @@
 module ChainedArrays
 
-# Write your package code here.
+import Base: size, getindex, setindex!, (:), first, last
+
+export ChainedVector
+
+struct ChainedVector{T, N, A<:AbstractVector{T}} <: AbstractVector{T}
+    chain::NTuple{N, A}
+end
+
+ChainedVector(vs::Vararg{A, N}) where {T, N, A<:AbstractVector{T}} = ChainedVector{T, N, A}(vs)
+
+size(c::ChainedVector) = .+(size.(c.chain)...)
+
+struct LinkIndex
+    link::Int
+    index::Int
+end
+
+getindex(c::ChainedVector, i::LinkIndex) = c.chain[i.link][i.index]
+setindex!(c::ChainedVector, x, i::LinkIndex) = c.chain[i.link][i.index] = x
+
+function tolinkindex(c::ChainedVector, i::Integer)
+    s::Int, l = 0, 0
+    while s < i
+        s += size(c.chain[l += 1], 1)
+    end
+    return LinkIndex(l, i - s + length(c.chain[l]))
+end
+
+struct LinkIndices
+    links::UnitRange{Int}
+    indices::UnitRange{Int}
+end
+
+first(i::LinkIndices) = LinkIndex(first(i.links), first(i.indices))
+last(i::LinkIndices) = LinkIndex(last(i.links), last(i.indices))
+
+(:)(l1::LinkIndex, l2::LinkIndex) = LinkIndices(l1.link:l2.link, l1.index:l2.index)
+
+function getindex(c::ChainedVector, i::LinkIndices)
+    length(i.links) == 1 && return c.chain[first(i).link][i.indices]
+    length(i.links) == 2 && return vcat(
+        c.chain[first(i).link][first(i).index:end],
+        c.chain[last(i).link][begin:last(i).index]
+    )
+
+    vs = Vector{eltype(c.chain)}(undef, length(i.links))
+    vs[begin] = c.chain[first(i).link][first(i).index:end]
+    vs[end] = c.chain[last(i).link][begin:last(i).index]
+    vs[begin + 1:end - 1] .= c.chain[first(i).link + 1:last(i).link - 1]
+    return vcat(vs...)
+end
+
+tolinkindex(c::ChainedVector, r::AbstractUnitRange) =
+    tolinkindex(c, first(r)):tolinkindex(c, last(r))
+
+getindex(c::ChainedVector, I) = getindex(c, tolinkindex(c, I))
+setindex!(c::ChainedVector, x, I) = setindex!(c, x, tolinkindex(c, I))
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using ChainedArrays
 using Test
 
 @testset "ChainedArrays.jl" begin
-    # Write your tests here.
+    v = ChainedVector([1, 2, 3], [4], [5, 6, 7, 8, 9, 10])
+
+    @test all(v .== 1:10)
+    @test collect(x for x in v) == 1:10
+
+    @test v[7] == 7
+
+    @test v[1:5] == 1:5
+
+    @test (v[3] = 10; v[3] == 10)
+
+    @test (v[4:10] .= 1:7; v[4:10] == 1:7)
 end
